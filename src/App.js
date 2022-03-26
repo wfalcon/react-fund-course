615d848c3462a0b913835f5a44ca06b4ce1e2984
Falcon <mail@sokolvlad.ru>
2022-03-26 18:04:33 +0300

    https://youtu.be/GNrdg3PzpJQ?t=2486

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-import React from "react";
-import Counter from "./components/Counter";
+import React, { useState } from "react";
+// import Counter from "./components/Counter";
+import Postitem from "./components/PostItem";
+import PostList from "./components/PostList";
+import './styles/App.css'
 
 function App() {
+  const [posts, setPosts] = useState([
+    { id: 1, title: 'Javascript', body: 'Description' },
+    { id: 2, title: 'Python', body: 'Description' },
+    { id: 3, title: 'Java', body: 'Description' },
+  ])
+  const [posts2, setPosts2] = useState([
+    { id: 1, title: 'Javascript 2', body: 'Description' },
+    { id: 2, title: 'Python 2', body: 'Description' },
+    { id: 3, title: 'Java 2', body: 'Description' },
+  ])
 
   return (
     <div className="App">
-      <Counter />
+      <PostList posts={posts} title="Список постов" />
+      <PostList posts={posts2} title="Список постов 1" />
     </div>
   );
 }
--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,22 +1,22 @@
 import React, { useState } from "react";
 
 const Counter = function () {
-    const [count, setCount] = useState(0)
-    function increment() {
-        setCount(count +1)
-        console.log(count)
-      }
-      function decrement() {
-        setCount(count -1)
-        console.log(count)
-      }
+  const [count, setCount] = useState(0)
+  function increment() {
+    setCount(count + 1)
+    console.log(count)
+  }
+  function decrement() {
+    setCount(count - 1)
+    console.log(count)
+  }
 
-    return (
-        <div>
-            <h1>{count}</h1>
-            <button onClick={increment}>increment</button>
-            <button onClick={decrement}>decrement</button>
-        </div>
-    )
+  return (
+    <div>
+      <h1>{count}</h1>
+      <button onClick={increment}>increment</button>
+      <button onClick={decrement}>decrement</button>
+    </div>
+  )
 }
 export default Counter;
